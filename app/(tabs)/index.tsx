17854f64c2fcea480f36e5cf4e58ab32e0bc9d77
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2025-12-12 14:34:50 +0100

    chore: update dependencies and add Tailwind CSS configuration
    - Added new dependencies: @expo/html-elements, @gluestack-ui/core, @gluestack-ui/utils, @legendapp/motion, nativewind, react-aria, react-stately, tailwind-variants.
    - Updated existing dependencies: react-native-reanimated, react-native-safe-area-context, react-native-worklets.
    - Added Tailwind CSS configuration file (tailwind.config.js) with custom theme and safelist.
    - Updated tsconfig.json to include path for tailwind.config.js and added nativewind-env.d.ts.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text>Timer Screen</Text>
+      <Text className="text-3xl">Timer Screen</Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from "expo-router";
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function RootLayout() {
   return (
-    <Stack>
+    
+    <GluestackUIProvider mode="dark">
+      <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
+    </GluestackUIProvider>
+  
   );
 }
